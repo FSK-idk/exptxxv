7cdfbe7b6c920416a88b5885fffe7757cbd010f2
fsk <vovarybkin666@gmail.com>
2025-10-27 10:12:08 +1000

    feat: add basic lexer

--- xxvc/src/main.cpp
+++ xxvc/src/main.cpp
@@ -1,10 +1,50 @@
 import std;
+import types;
+import compiler;
 
-using i32 = std::int32_t;
+
+// xxv grammar
+//
+// module
+//     ::= ( ( declaration | definition ) ';' )*
+// declaration
+//     ::= function_declaration
+//     ::= extern_function_declaration
+// function_declaration
+//     ::= 'fn' identifier ':' '(' ( identifier ( ',' identifier )* )? ')'
+// extern_function_declaration
+//     ::= 'extern' function_declaration
+// definition
+//     ::= function_definition
+// function_definition
+//     ::= function_declaration '{' ( statement ';' )* '}'
+// statement
+//     ::= return_statement
+// return_statement
+//     ::= 'return' number
+//
+// example
+//
+// extern
+// fn print: (x);
+//
+// fn main: () {
+//     return 0;
+// };
 
 
 auto main() -> i32 {
-    std::println("Hello World!");
+    std::string program = R"(
+        fn main: () {
+            return 0;
+        };
+    )";
+
+    std::println("program: {}", program);
+
+    Compiler compiler;
+    
+    compiler.exec(std::move(program));
 
     std::println("Returning 0...");
     return 0;
